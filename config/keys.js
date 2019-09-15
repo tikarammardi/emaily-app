b6ab393c15ffea39e5017995628c1cd99b7ccbbb
Tikaram Mardi <tikarammardi007@gmail.com>
2019-09-15 12:55:23 +0530

    production is set up

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+//figure out what set of credentials to return
+
+if (process.env.NODE_ENV === 'production') {
+  //return prod keys
+  module.exports = require('./prod');
+} else {
+  //return dev keys
+  module.exports = require('./dev');
+}
--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/User');
+require('./services/passport');
 
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send({ hi: 'Mew CAt' });
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
+const app = express();
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
