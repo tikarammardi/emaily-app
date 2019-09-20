3971ed715280c90737c9c9de21c9ded2fc46abad
Tikaram Mardi <tikarammardi007@gmail.com>
2019-09-20 13:57:11 +0530

    heroku config done

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Express serving up production assets
+  app.use(express.static('client/build'));
+  //Express serving up the index.html file if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server Listening on port ${PORT}`));
