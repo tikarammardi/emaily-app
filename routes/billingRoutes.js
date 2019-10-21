2e682a6778bf986192fb1873348fbb7acf3bb5fd
Tikaram Mardi <tikarammardi007@gmail.com>
2019-10-21 16:58:16 +0530

    fixing bugs

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -9,7 +9,7 @@ module.exports = app => {
       description: '$5 for 5 credits',
       source: req.body.id
     });
-    req.user.credits += 6;
+    req.user.credits += 5;
     const user = await req.user.save();
     res.send(user);
   });
